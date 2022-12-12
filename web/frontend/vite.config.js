40b5962c4feff4ddc56da40323f6f2257d069667
Umang <umang@vedaha.com>
2022-12-12 09:00:02 +0530

    chk1

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig,loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import https from "https";
 import react from "@vitejs/plugin-react";
-
-process.env = {...process.env, ...loadEnv('production', process.cwd(),'SHOPIFY_API_KEY')};
 
 console.log(process.env.SHOPIFY_API_KEY);
 
