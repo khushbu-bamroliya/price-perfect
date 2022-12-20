68fd6630f93520c30002d6b0f0c2f84f17480f01
rutvik <rutvikdiyora111@gmail.com>
2022-12-20 17:47:53 +0530

    old commit

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,34 +1,42 @@
+// import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import Pricetest from "./Component/Pricetest";
+
+// export default function App() {
+
+//   return (
+//     <div>
+//       <Pricetest />
+//     </div>
+//   );
+// }
+
+
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Pricetest from "./Component/Pricetest";
 import YourTest from "./Component/YourTest";
 import { Route, Routes } from "react-router-dom";
 import WelcomeBack from "./Component/WelcomeBack";
 import CreateTest from "./Component/CreateTest";
 import CreateTestPage2 from "./Component/CreateTestPage2";
-import SignUp from "./Component/SignUp"
-import Revenue from "./Component/Revenue";
-import HomeDashboard from "./Component/HomeDashboard";
-import WelcomePage from "./Component/WelcomePage";
+
 
 export default function App() {
 
   return (
     <div>
-      <Routes>
-        <Route element={<WelcomePage />} path='/'/>
-        <Route element={<SignUp />} path='/signup'/>
-        <Route element={<HomeDashboard /> } path='/homeDashboard' />
-      </Routes>
-      {/* <WelcomePage /> */}
-      {/* <SignUp /> */}
-      {/* <Revenue /> */}
-      {/* <ResultPage /> */}
-      {/* <homeDashboard /> */}
-      {/* <YourTest /> */}
-      {/* <WelcomeBack /> */}
-      {/* <CreateTest /> */}
-      {/* <CreateTestPage2 /> */}
+      {/* <Routes> */}
+        {/* <Route exact path="/test" element={<Test/> } /> */}
+        {/* <Route exact path="/createtest" element={<CreateTest/> }  />
+      </Routes> */}
+      <Pricetest />
+      <YourTest />
+      <WelcomeBack />
+      <CreateTest />
+      <CreateTestPage2 />
     </div>
   );
 }
