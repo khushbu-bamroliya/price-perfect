eaae85723cee3123c65b26298b92f23f891a29e8
rutvik <rutvikdiyora111@gmail.com>
2022-12-21 14:55:30 +0530

    old working code

---
+++ web/frontend/src/Component/Pricetest.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import WelcomePage from './WelcomePage.js'
+
+export default function Pricetest() {
+    return(
+        <>
+        <WelcomePage />
+        </>
+    )
+}
