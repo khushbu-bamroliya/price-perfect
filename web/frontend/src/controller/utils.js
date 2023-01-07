8926eba6a9f016968b3a5f8fa776098922bb7de1
Umang <umang@vedaha.com>
2023-01-07 13:46:39 +0530

    changes 2

--- web/frontend/src/controller/utils.js
+++ web/frontend/src/controller/utils.js
@@ -1,3 +1,4 @@
-const getApiUrl = process.env.REACT_NODE_ENV === "production" ? "/" : "http://localhost:8081";
+// const getApiUrl = process.env.REACT_NODE_ENV === "production" ? "/" : "http://localhost:8081";
+const getApiUrl = process.env.REACT_NODE_ENV === "production" ? "/" : "/";
 
 export default getApiUrl; 
